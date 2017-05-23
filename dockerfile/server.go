97b51093919dc74cefe9556affb3290d7c44fbfe
Max Schoening <max@max.wtf>
2017-05-22 20:11:07 -0700

    Syntax error

--- dockerfile/server.go
+++ dockerfile/server.go
@@ -9,7 +9,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
   io.WriteString(w, "Hello World")
 }
 
-funct main() {
+func main() {
   http.HandleFunc("/", hello)
   http.listenAndServe(":3000", nil)
 }
